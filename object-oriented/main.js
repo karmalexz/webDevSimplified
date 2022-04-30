123c4b63597f6f7467a9df268b7fe33512e36ae5
karmalexz <49818239+karmalexz@users.noreply.github.com>
2022-04-30 21:19:52 +1000

    prototype

--- object-oriented/main.js
+++ object-oriented/main.js
@@ -1,8 +1,22 @@
-function CreateObj (name, age){
+// function CreateObj (name, age){
+//   this.name=name
+//   this.age=age
+//   this.sayHi=function(){console.log("say hi")}
+// }
+
+// let obj1=new CreateObj("alex",18)
+// let obj2=new CreateObj("eric",19)
+// console.log(obj1,obj2)
+
+function Person (name, age){
   this.name=name
   this.age=age
 }
-
-let obj1=new CreateObj("alex",18)
-let obj2=new CreateObj("eric",19)
-console.log(obj1,obj2)
+Person.prototype.sayHi=function(){
+  console.log("say hi")
+}
+let person1=new Person("alex",16)
+let person2=new Person("eric",17)
+person1.sayHi()
+person2.sayHi()
+console.log(person1.name)
