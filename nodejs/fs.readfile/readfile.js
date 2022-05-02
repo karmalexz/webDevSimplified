dda2738fa50f3ef095bf3fc5d4762b6a56b06f91
karmalexz <49818239+karmalexz@users.noreply.github.com>
2022-05-03 01:44:39 +1000

    nodejs

---
+++ nodejs/fs.readfile/readfile.js
@@ -0,0 +1,15 @@
+const fs =require ('fs')
+// fs.readFile('1.txt','utf-8',(err,dataStr)=>{
+//     console.log(err)
+//     console.log('-------')
+//     console.log(dataStr)
+// })
+// if read successfully, the err is null 
+// if read failed, the err is an error object, dataStr is undefined. 
+
+fs.readFile('11.txt','utf8',(err,dataStr)=>{
+    if(err){
+        return console.log("failed!" +  err.message)
+    }
+    console.log('successful!'+dataStr)
+})
